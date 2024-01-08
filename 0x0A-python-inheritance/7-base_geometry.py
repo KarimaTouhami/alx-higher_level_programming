6c82952fe8972e0323850fba1a90b47f95ac6f7a
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-01-08 10:14:17 +0000

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """The BaseGeometry class."""
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
