1bb4fc82be663d69068a89f2816da4b5633dc8fc
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-01-13 13:59:52 +0000

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,13 +78,17 @@ class Rectangle(Base):
         """ Calculates and returns the area of the rectangle """
         return self.width * self.height
 
+    def display(self):
+        """ Displays the rectangle using '#' character """
+        for _ in range(self.height):
+            print("#" * self.width)
+
 
 if __name__ == "__main__":
-    r1 = Rectangle(3, 2)
-    print(r1.area())
+    r1 = Rectangle(4, 6)
+    r1.display()
 
-    r2 = Rectangle(2, 10)
-    print(r2.area())
+    print("---")
 
-    r3 = Rectangle(8, 7, 0, 0, 12)
-    print(r3.area())
+    r2 = Rectangle(2, 2)
+    r2.display()
